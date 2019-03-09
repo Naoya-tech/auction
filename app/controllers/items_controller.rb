f849df4b80442f546aec64ffcd5bd50b419ae3ed
Naoya-tech <naoya.techwork@gmail.com>
2019-03-09 17:34:03 +0900

    create create

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,6 +12,14 @@ def new
 end
 
 def create
+  @item = Item.new(item_params)
+  @item.save
+  redirect_to @item
+end
+
+private
+def item_params
+  params.require(:item).permit(:name, :price, :seller, :description, :email, :image_url)
 end
 
 end
