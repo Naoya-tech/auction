4b788c194a94ff935a16acb1908330ce77238f43
Naoya-tech <naoya.techwork@gmail.com>
2019-03-09 17:44:47 +0900

    create UPDATE

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,15 +7,25 @@ class ItemsController < ApplicationController
     @items = Item.all
   end
 
-def new
-  @item = Item.new 
-end
+  def new
+    @item = Item.new 
+  end
 
-def create
-  @item = Item.new(item_params)
-  @item.save
-  redirect_to @item
-end
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to @item
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to @item
+  end
 
 private
 def item_params
